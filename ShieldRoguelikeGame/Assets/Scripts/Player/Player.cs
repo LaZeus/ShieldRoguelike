6d622cef0c02176ee96a2ba709e6ef4b7d262a3e
LW\LW <ladimitr01@gmail.com>
2018-10-11 18:31:05 +0300

    Basic Player's Controls

---
+++ ShieldRoguelikeGame/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : PlayerController {
+
+    private void Awake()
+    {
+        Initialization();
+    }
+	
+	private void FixedUpdate()
+    {
+        PlayerControls();
+	}
+
+    private void PlayerControls()
+    {
+        if (ControlActions != null)
+            ControlActions();
+    }
+
+}
---
+++ ShieldRoguelikeGame/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    protected enum States { Normal, Dying }
+
+    protected delegate void Actions();
+
+    #region variables
+
+    protected States mState;
+
+    private Rigidbody2D rb;
+
+    protected Actions ControlActions;
+
+    [SerializeField]
+    private float speed;
+
+    #endregion
+
+    #region Init
+
+    protected void Initialization()
+    {
+        if (rb == null)
+            rb = GetComponent<Rigidbody2D>();
+
+        ControlActions = Moving;
+    }
+
+    #endregion
+
+    protected void Moving()
+    {
+        float Input_x = Input.GetAxisRaw("Horizontal");
+        float Input_y = Input.GetAxisRaw("Vertical");
+
+        if (mState == States.Normal)
+        {
+            rb.velocity = new Vector2(Input_x, Input_y).normalized * speed;
+        }
+    }
+
+}
