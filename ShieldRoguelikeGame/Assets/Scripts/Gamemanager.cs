28cf0ce4b05e1119d07776a29318015a0269ec36
LW\LW <ladimitr01@gmail.com>
2018-10-16 20:55:50 +0300

    added object pools package from asset store

---
+++ ShieldRoguelikeGame/Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using EZObjectPools;
+
+public class Gamemanager : MonoBehaviour {
+
+    public EZObjectPool objectPool;
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
