1f397e095dce6017da7548c46bc203744d610645
LW\LW <ladimitr01@gmail.com>
2018-10-18 20:31:44 +0300

    minor changes

---
+++ ShieldRoguelikeGame/Assets/Scripts/Player/PlayerProjectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.tag == "Wall")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
